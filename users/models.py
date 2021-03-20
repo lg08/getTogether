1ff90779facf86a65c77eb7af16f907fcd7952fd
Lucas Gen <lg08@pop-os.localdomain>
2021-03-20 15:11:42 -0400

    sets up user models.py and makes migrations to database

---
+++ users/models.py
@@ -0,0 +1,59 @@
+from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
+
+# Create your models here.
+
+class Profile(models.Model):
+    user = models.OneToOneField(
+        User,
+        null=True,
+        blank=True,
+        on_delete=models.CASCADE
+    )
+    full_name = models.CharField(null=False, blank=False,
+                                 max_length=50, default="full name here")
+    location = models.IntegerField(default=95818) # TODO: change this
+    # to better location system
+
+    # got this stuff from here: https://simpleisbetterthancomplex.com/tutorial/
+    # 2016/07/22/how-to-extend-django-user-model.html
+    @receiver(post_save, sender=User)
+    def create_user_profile(sender, instance, created, **kwargs):
+        if created:
+            Profile.objects.create(user=instance)
+
+    @receiver(post_save, sender=User)
+    def save_user_profile(sender, instance, **kwargs):
+        instance.profile.save()
+
+class Channel(models.Model):
+    title = models.CharField(null=False, blank=False,
+                             max_length=50, default="title here")
+    location = models.IntegerField(default=95818) # TODO: change this
+    # to better location system
+
+class Post(models.Model):
+    creater = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE
+    )
+
+class Event(models.Model):
+    title = models.CharField(null=False, blank=False,
+                             max_length=50, default="title here")
+    description = models.CharField(
+        null=False, blank=False,
+        max_length=50,
+        default="description here"
+    )
+    location = models.IntegerField(default=95818) # TODO: change this
+    # to better location system
+    start_time = models.DateTimeField(
+        auto_now=True
+    )
+    end_time = models.DateTimeField(
+        auto_now=False
+    )
