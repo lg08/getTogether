dabeed26bbd717a0c058b460dc8b20b00d5d1aed
Lucas Gen <lg08@pop-os.localdomain>
2021-03-22 15:51:43 -0400

    adds proper imports

--- channels/views.py
+++ channels/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 
 from posts.models import Post
 from .models import Channel
