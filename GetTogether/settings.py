d604c876f2c51cd3d97080d589ac7aab02357d9f
ykocaman <ykocaman@princeton.edu>
2021-04-08 21:06:45 -0400

    adds STATIC_ROOT in settings.py

--- GetTogether/settings.py
+++ GetTogether/settings.py
@@ -128,10 +128,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-
+STATIC_ROOT = 
 # tells django where to go to look for static files
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'staticfiles'),
 ]
 
 
