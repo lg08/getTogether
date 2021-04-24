baff3a0db569b406d2c5c269b168d3f66ccd9851
ykocaman <ykocaman@princeton.edu>
2021-04-23 22:43:07 -0400

    fixed typo

--- channels/views.py
+++ channels/views.py
@@ -139,7 +139,7 @@ def main_feed(request):
 
     upvotes = []
     downvotes = []
-    for i, post in enumerate(all_main_feed_postsf):
+    for i, post in enumerate(all_main_feed_posts):
         upvotes.append(str(Upvote.objects.filter(user=request.user, post=all_main_feed_posts[i])))
         downvotes.append(str(Downvote.objects.filter(user=request.user, post=all_main_feed_posts[i])))
 
