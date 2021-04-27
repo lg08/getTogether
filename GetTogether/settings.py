7562be966ce136600fdb1d6b8acedb3d967a035c
Yusuf Kocaman <yusuf@dynamic-oit-vapornet100-10-9-53-42.princeton.edu>
2021-04-26 23:25:44 -0400

    made forgot password

--- GetTogether/settings.py
+++ GetTogether/settings.py
@@ -81,6 +81,16 @@ TEMPLATES = [
     },
 ]
 
+# DEVELOPMENT EMAIL. SWITCH TO SMTP 
+EMAIL_USE_TLS = True
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'gettogetherprinceton@gmail.com'
+EMAIL_HOST_PASSWORD = 'ejdshkrxlupapzmz'
+DEFAULT_FROM_EMAIL = 'gettogetherprinceton@gmail.com'
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+SERVER_EMAIL = 'gettogetherprinceton@gmail.com'
+
 WSGI_APPLICATION = 'GetTogether.wsgi.application'
 
 
--- users/urls.py
+++ users/urls.py
@@ -30,5 +30,25 @@ urlpatterns = [
         views.change_location,
         name='change_location'
     ),
+    path(
+        "password_reset/",
+        auth_views.PasswordResetView.as_view(template_name='users/passwordReset.html'), 
+        name='password_reset',
+    ),
+    path(
+        "password_reset/done/",
+        auth_views.PasswordResetDoneView.as_view(template_name='users/passwordResetDone.html'), 
+        name='password_reset_done',
+    ),
+    path(
+        "reset/<uidb64>/<token>/",
+        auth_views.PasswordResetConfirmView.as_view(template_name='users/reset.html'), 
+        name='password_reset_confirm'
+    ),
+    path(
+        "reset/done/", 
+        auth_views.PasswordResetCompleteView.as_view(template_name='users/resetDone.html'), 
+        name='password_reset_complete'
+    )
 
 ]
